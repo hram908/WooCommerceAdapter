9cee953e8c67569d360a3b7362af487d9154f0cb
Ezequiel <ezequielmatalon@gmail.com>
2014-08-25 11:25:36 -0300

    Finishing Orders service

--- tuvieja/src/main/java/com/prous/trdnpdf/tuvieja/OrdersFacade.java
+++ tuvieja/src/main/java/com/prous/trdnpdf/tuvieja/OrdersFacade.java
@@ -5,19 +5,24 @@ import java.util.List;
 import com.joybug.pojo.Order;
 
 public interface OrdersFacade {
-	public enum Statuses{
-		pending("pending"), onHold("on-hold"), processing("processing"), completed("completed"), refunded("refunded"), failed("failed"), cancelled("cancelled");
-		
+	public enum Statuses {
+		pending("pending"), onHold("on-hold"), processing("processing"), completed("completed"), refunded("refunded"), failed(
+				"failed"), cancelled("cancelled");
+
 		private final String statusName;
-		private Statuses(String status){
-			statusName=status;
+
+		private Statuses(String status) {
+			statusName = status;
 		}
+
 		public String getStatusName() {
 			return statusName;
 		}
 	}
-	
-	public List<Order> getOrdersWithStatus(Statuses status) throws Exception;
-	public Order getOrder(String id) throws Exception;
-	public void updateOrderStatus(Statuses status) throws Exception;
+
+	public List<Order> getOrdersWithStatus(Statuses status, List<String> fields) throws Exception;
+
+	public Order getOrder(String id, List<String> fields) throws Exception;
+
+	public void updateOrderStatus(Statuses status, String id) throws Exception;
 }
--- tuvieja/src/main/java/com/prous/trdnpdf/tuvieja/OrdersFacadeImpl.java
+++ tuvieja/src/main/java/com/prous/trdnpdf/tuvieja/OrdersFacadeImpl.java
@@ -1,43 +1,51 @@
 package com.prous.trdnpdf.tuvieja;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.lang3.StringUtils;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import com.joybug.pojo.Order;
 import com.joybug.pojo.Orders;
 
-public class OrdersFacadeImpl implements OrdersFacade{
+public class OrdersFacadeImpl implements OrdersFacade {
 	private final String secret = "cs_c184cb8055b47a1df6242b4dcc9318f9";
 	private final String key = "ck_db853a745d378b0944342c0ecc96535a";
 	private final String url = "http://joybug.net";
-	
+
 	private final WooCommerceClientHelper wooClient = WooCommerceClientHelper.getInstance(secret, key, url);
-	
+
 	@Override
-	public List<Order> getOrdersWithStatus(Statuses status) throws Exception{
+	public List<Order> getOrdersWithStatus(Statuses status, List<String> fields) throws Exception {
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new NameValuePair("status",status.getStatusName()));
-		new Gson().fromJson(wooClient._make_api_call("order", params, "GET"),Orders.class).getOrders();
+		params.add(new NameValuePair("status", status.getStatusName()));
+		if (fields != null && !fields.isEmpty()) {
+			params.add(new NameValuePair("fields", StringUtils.join(fields, ",")));
+		}
+		return new Gson().fromJson(wooClient._make_api_call("order", params, "GET"), Orders.class).getOrders();
 	}
 
 	@Override
-	public Order getOrder(String id) throws Exception{
+	public Order getOrder(String id, List<String> fields) throws Exception {
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new NameValuePair("id",id));
+		params.add(new NameValuePair("id", id));
+		if (fields != null && !fields.isEmpty()) {
+			params.add(new NameValuePair("fields", StringUtils.join(fields, ",")));
+		}
 		return new Gson().fromJson(wooClient._make_api_call("order", params, "GET"), Orders.class).getOrder();
 	}
 
 	@Override
-	public void updateOrderStatus(Statuses status) {
-		// TODO Auto-generated method stub
-		
+	public void updateOrderStatus(Statuses status, String id) throws Exception {
+		if (getOrder(id, null) != null) {
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			params.add(new NameValuePair("id", id));
+			params.add(new NameValuePair("status", status.getStatusName()));
+			wooClient._make_api_call("order", params, "PUT");
+		}
+
 	}
 
 }
