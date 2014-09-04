c0aca9f4e299e50dd7e3e5e97d3ca9c7cbf3c441
Ezequiel <ezequielmatalon@gmail.com>
2014-09-04 18:43:51 -0300

    Adding get multiple values out of meta

--- WooCommerceClientForJ/src/main/java/com/wooCommerce/forJ/pojo/v2/Metum.java
+++ WooCommerceClientForJ/src/main/java/com/wooCommerce/forJ/pojo/v2/Metum.java
@@ -1,5 +1,8 @@
-
 package com.wooCommerce.forJ.pojo.v2;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.annotation.Generated;
 
@@ -10,55 +13,68 @@ import com.google.gson.annotations.Expose;
 @Generated("org.jsonschema2pojo")
 public class Metum {
 
-    @Expose
-    private String key;
-    @Expose
-    private String label;
-    @Expose
-    private String value;
+	@Expose
+	private String key;
+	@Expose
+	private String label;
+	@Expose
+	private String value;
 
-    public String getKey() {
-        return key;
-    }
+	public String getKey() {
+		return key;
+	}
 
-    public void setKey(String key) {
-        this.key = key;
-    }
+	public void setKey(String key) {
+		this.key = key;
+	}
 
-    public Metum withKey(String key) {
-        this.key = key;
-        return this;
-    }
+	public Metum withKey(String key) {
+		this.key = key;
+		return this;
+	}
 
-    public String getLabel() {
-        return label;
-    }
+	public String getLabel() {
+		return label;
+	}
 
-    public void setLabel(String label) {
-        this.label = label;
-    }
+	public void setLabel(String label) {
+		this.label = label;
+	}
 
-    public Metum withLabel(String label) {
-        this.label = label;
-        return this;
-    }
+	public Metum withLabel(String label) {
+		this.label = label;
+		return this;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public void setValue(String value) {
-        this.value = value;
-    }
+	public void setValue(String value) {
+		this.value = value;
+	}
 
-    public Metum withValue(String value) {
-        this.value = value;
-        return this;
-    }
+	public Metum withValue(String value) {
+		this.value = value;
+		return this;
+	}
 
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 
+	public Integer getIntegerValue() {
+		return Integer.valueOf(value);
+	}
+
+	public Date getDateValue(String pattern) {
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		try {
+			return sdf.parse(value);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+	}
 }
